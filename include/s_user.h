32bb616fd585fcf44ebc4f6b59e0e438687acec5
knight <knight@82007160-df01-0410-b94d-b575c5fd34c7>
2005-10-12 17:11:55 +0000

    - More GCC4 fixes
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid@116 82007160-df01-0410-b94d-b575c5fd34c7

--- include/s_user.h
+++ include/s_user.h
@@ -31,6 +31,7 @@
 
 struct Client;
 struct AccessItem;
+struct ConfItem;
 
 extern int MaxClientCount;     /* GLOBAL - highest number of clients     */
 extern int MaxConnectionCount; /* GLOBAL - highest number of connections */
