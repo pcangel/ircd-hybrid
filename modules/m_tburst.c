31ac1140fcb5335a1750eeeb93e58288876dd38b
adx <adx@82007160-df01-0410-b94d-b575c5fd34c7>
2005-10-13 11:02:49 +0000

    - renamed m_tb.c to m_tburst.c [could not resist]
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid@123 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_tburst.c
+++ modules/m_tburst.c
@@ -25,7 +25,7 @@
  *  IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *
- *  $Id: m_tb.c,v 1.25 2005/06/23 16:04:39 michael Exp $
+ *  $Id: m_tb.c 122 2005-10-13 10:57:26Z adx $
  */
 
 #include "stdinc.h"
@@ -36,12 +36,11 @@
 #include "send.h"
 #include "msg.h"
 #include "modules.h"
-#include "hook.h"
 #include "hash.h"
 #include "s_serv.h"
 #include "s_conf.h"
 
-
+static void ms_tb(struct Client *, struct Client *, int, char *[]);
 static void ms_tburst(struct Client *, struct Client *, int, char *[]);
 static void set_topic(struct Client *, struct Channel *, time_t, char *, char *);
 
@@ -50,10 +49,19 @@ struct Message tburst_msgtab = {
   {m_ignore, m_ignore, ms_tburst, m_ignore, m_ignore, m_ignore}
 };
 
+struct Message tb_msgtab = {
+  "TB", 0, 0, 0, 0, MFLG_SLOW, 0,
+  {m_ignore, m_ignore, ms_tb, m_ignore, m_ignore, m_ignore}
+};
+
 #ifndef STATIC_MODULES
+
 void
 _modinit(void)
 {
+  mod_add_cmd(&tb_msgtab);
+  add_capability("TB", CAP_TB, 1);
+
   mod_add_cmd(&tburst_msgtab);
   add_capability("TBURST", CAP_TBURST, 1);
 }
@@ -61,16 +69,19 @@ _modinit(void)
 void
 _moddeinit(void)
 {
+  mod_del_cmd(&tb_msgtab);
+  delete_capability("TB");
+
   mod_del_cmd(&tburst_msgtab);
   delete_capability("TBURST");
 }
 
-const char *_version = "$Revision: 1.25 $";
+const char *_version = "$Revision: 122 $";
+
 #endif /* !STATIC_MODULES */
 
-
 /* ms_tburst()
- * 
+ *
  *      parv[0] = sender prefix
  *      parv[1] = channel timestamp
  *      parv[2] = channel
@@ -99,22 +110,76 @@ ms_tburst(struct Client *client_p, struct Client *source_p,
     set_topic(source_p, chptr, oldtopicts, parv[4], parv[5]);
 }
 
+/* ms_tb()
+ * 
+ *      parv[0] = sender prefix
+ *      parv[1] = channel name
+ *      parv[2] = topic timestamp
+ *      parv[3] = topic setter OR topic itself if parc == 4
+ *      parv[4] = topic itself if parc == 5
+ */
+#define tb_channel      parv[1]
+#define tb_topicts_str  parv[2]
+
+static void
+ms_tb(struct Client *client_p, struct Client *source_p, int parc, char *parv[])
+{
+  struct Channel *chptr;
+  time_t tb_topicts = atol(tb_topicts_str);
+  char *tb_whoset = NULL;
+  char *tb_topic = NULL;
+
+  if ((chptr = hash_find_channel(tb_channel)) == NULL)
+    return;
+
+  if (parc == 5)
+  {
+    tb_whoset = parv[3];
+    tb_topic = parv[4];
+  }
+  else
+  {
+    tb_whoset = source_p->name;
+    tb_topic = parv[3];
+  }
+
+  set_topic(source_p, chptr, tb_topicts, tb_whoset, tb_topic);
+}
+
+/*
+ * set_topic
+ *
+ * inputs	- source_p pointer
+ *		- channel pointer
+ *		- topicts to set
+ *		- who to set as who doing the topic
+ *		- topic
+ * output	- none
+ * Side effects	- simply propagates topic as needed
+ * little helper function, could be removed
+ */
 static void
 set_topic(struct Client *source_p, struct Channel *chptr, 
-          time_t oldtopicts, char *topicwho, char *topic)
+          time_t topicts, char *topicwho, char *topic)
 {
-  set_channel_topic(chptr, topic, topicwho, oldtopicts);
+  set_channel_topic(chptr, topic, topicwho, topicts);
 
-  /* Only send TOPIC to channel if it's different */
+    /* Only send TOPIC to channel if it's different */
   if (chptr->topic == NULL || strcmp(chptr->topic, topic))
-    sendto_channel_local(ALL_MEMBERS, chptr, ":%s TOPIC %s :%s",
+    sendto_channel_local(ALL_MEMBERS, NO, chptr, ":%s TOPIC %s :%s",
                          ConfigServerHide.hide_servers ? me.name : source_p->name,
                          chptr->chname, chptr->topic == NULL ? "" : chptr->topic);
 
   sendto_server(source_p, NULL, chptr, CAP_TBURST, NOCAPS, NOFLAGS,
-		":%s TBURST %lu %s %lu %s :%s",
-		me.name, (unsigned long)chptr->channelts, chptr->chname,
-		(unsigned long)chptr->topic_time, 
+                ":%s TBURST %lu %s %lu %s :%s",
+                me.name, (unsigned long)chptr->channelts, chptr->chname,
+                (unsigned long)chptr->topic_time,
+                chptr->topic_info == NULL ? "" : chptr->topic_info,
+                chptr->topic == NULL ? "" : chptr->topic);
+  sendto_server(source_p, NULL, chptr, CAP_TB, CAP_TBURST, NOFLAGS,
+                ":%s TB %s %lu %s :%s",
+                me.name, chptr->chname,
+                (unsigned long)chptr->topic_time, 
                 chptr->topic_info == NULL ? "" : chptr->topic_info,
                 chptr->topic == NULL ? "" : chptr->topic);
 }
