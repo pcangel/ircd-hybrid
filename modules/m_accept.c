2668f838645945a8322fa60798a99c4d82c30845
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2005-11-05 17:54:04 +0000

    - Got rid of build_nuh_list() - Readd ability of showing ACCEPT list via "ACCEPT *" as described in umodeg.txt
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid@239 82007160-df01-0410-b94d-b575c5fd34c7

--- modules/m_accept.c
+++ modules/m_accept.c
@@ -25,7 +25,6 @@
 #include "stdinc.h"
 #include "handlers.h"
 #include "client.h"
-#include "hash.h"       /* for find_client() */
 #include "ircd.h"
 #include "numeric.h"
 #include "s_conf.h"
@@ -37,7 +36,6 @@
 #include "modules.h"
 
 static void m_accept(struct Client *, struct Client *, int, char *[]);
-static void build_nuh_list(struct Client *, char *, char *, char *);
 static void add_accept(const char *, const char *, const char *, struct Client *);
 static void list_accepts(struct Client *);
 
@@ -65,7 +63,7 @@ const char *_version = "$Revision$";
 /*
  * m_accept - ACCEPT command handler
  *      parv[0] = sender prefix
- *      parv[1] = servername
+ *      parv[1] = list of masks to be accepted or removed
  */
 static void
 m_accept(struct Client *client_p, struct Client *source_p,
@@ -74,109 +72,69 @@ m_accept(struct Client *client_p, struct Client *source_p,
   struct Accept *accept = NULL;
   char *mask, *nick, *user, *host;
   char *p = NULL;
-  char addbuf[IRCD_BUFSIZE] = { '\0' };
-  char delbuf[IRCD_BUFSIZE] = { '\0' };
-  int accept_num = 0;
+  unsigned int accept_num = 0;
   
-  if (parc < 2)
+  if (parc < 2 || !irccmp(parv[1], "*"))
   {
     list_accepts(source_p);
     return;
   }
 
-  build_nuh_list(source_p, addbuf, delbuf, parv[1]);
-
   /* parse the delete list */
-  for (mask = strtoken(&p, delbuf, ","); mask != NULL;
-       mask = strtoken(&p, NULL,   ","))
+  for (mask = strtoken(&p, parv[1], ","); mask != NULL;
+       mask = strtoken(&p,    NULL, ","))
   {
-    /* user isn't on clients accept list */
-    split_nuh(mask, &nick, &user, &host);
-
-    if ((accept = find_accept(nick, user, host, source_p, 0)) == NULL)
+    if (*mask == '-' && *++mask != '\0')
     {
-      sendto_one(source_p, form_str(ERR_ACCEPTNOT),
-                 me.name, source_p->name, nick, user, host);
+      split_nuh(mask, &nick, &user, &host);
+
+      if ((accept = find_accept(nick, user, host, source_p, 0)) == NULL)
+      {
+        sendto_one(source_p, form_str(ERR_ACCEPTNOT),
+                   me.name, source_p->name, nick, user, host);
+        MyFree(nick);
+        MyFree(user);
+        MyFree(host);
+        continue;
+      }
+
+      del_accept(accept, source_p);
+
       MyFree(nick);
       MyFree(user);
       MyFree(host);
-      continue;
     }
-
-    del_accept(accept, source_p);
-
-    MyFree(nick);
-    MyFree(user);
-    MyFree(host);
-  }
-
-  /* get the number of accepts they have */ 
-  accept_num = dlink_list_length(&source_p->localClient->acceptlist);
-
-  /* parse the add list */
-  for (mask = strtoken(&p, addbuf, ","); mask;
-       mask = strtoken(&p, NULL,   ","), ++accept_num)
-  {
-     /* user is already on clients accept list */
-    split_nuh(mask, &nick, &user, &host);
-
-    if ((accept = find_accept(nick, user, host, source_p, 0)) != NULL)
+    else if (*mask != '\0')
     {
-      sendto_one(source_p, form_str(ERR_ACCEPTEXIST),
-                 me.name, source_p->name, nick, user, host);
+      split_nuh(mask, &nick, &user, &host);
+
+      if ((accept = find_accept(nick, user, host, source_p, 0)) != NULL)
+      {
+        sendto_one(source_p, form_str(ERR_ACCEPTEXIST),
+                   me.name, source_p->name, nick, user, host);
+        MyFree(nick);
+        MyFree(user);
+        MyFree(host);
+        continue;
+      }
+
+      if (dlink_list_length(&source_p->localClient->acceptlist) >=
+          ConfigFileEntry.max_accept)
+      {
+        sendto_one(source_p, form_str(ERR_ACCEPTFULL),
+                   me.name, source_p->name);
+        MyFree(nick);
+        MyFree(user);
+        MyFree(host);
+        return;
+      }
+
+      add_accept(nick, user, host, source_p);
       MyFree(nick);
       MyFree(user);
       MyFree(host);
-      continue;
+      ++accept_num;
     }
-
-    if (accept_num >= ConfigFileEntry.max_accept)
-    {
-      sendto_one(source_p, form_str(ERR_ACCEPTFULL),
-                 me.name, source_p->name);
-      MyFree(nick);
-      MyFree(user);
-      MyFree(host);
-      return;
-    }
-
-    add_accept(nick, user, host, source_p);
-    MyFree(nick);
-    MyFree(user);
-    MyFree(host);
-  }
-}
-
-/* build_nuh_list()
- *
- * input	- pointer to client
- *		- pointer to addbuffer
- *		- pointer to remove buffer
- *		- pointer to list of nicks
- *		- pointer to list of nicks!user@host
- * output	- 
- * side effects - addbuf/delbuf are modified to give valid masks
- */
-static void
-build_nuh_list(struct Client *source_p, char *addbuf,
-               char *delbuf, char *inbuf)
-{
-  char *mask = NULL;
-  char *p = NULL;
-  char *buf_p = NULL;
-
-  /* build list of nuh to add into addbuf, nuh to remove in delbuf */
-  for (mask = strtoken(&p, inbuf, ","); mask; 
-       mask = strtoken(&p,  NULL, ","))
-  {
-    if (*mask == '-')
-      buf_p = delbuf, ++mask;
-    else
-      buf_p = addbuf;
-
-    if (*buf_p != '\0')
-      strlcat(buf_p, ",", IRCD_BUFSIZE);
-    strlcat(buf_p, mask, IRCD_BUFSIZE);
   }
 }
 
