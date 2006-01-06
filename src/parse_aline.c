52d0b52002e6c3979c76b2d9c895fb720a288295
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2006-01-06 16:51:28 +0000

    - Cleaned up find_user_host a bit more, also added support   for "KLINE somehost.edu"
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid@360 82007160-df01-0410-b94d-b575c5fd34c7

--- src/parse_aline.c
+++ src/parse_aline.c
@@ -281,7 +281,8 @@ static int
 find_user_host(struct Client *source_p, char *user_host_or_nick,
                char *luser, char *lhost, unsigned int flags)
 {
-  const struct Client *target_p = NULL;
+  char lnick[NICKLEN];
+  struct split_nuh_item nuh;
 
   if (lhost == NULL)
   {
@@ -289,37 +290,31 @@ find_user_host(struct Client *source_p, char *user_host_or_nick,
     return 1;
   }
 
-  if ((hostp = strchr(user_host_or_nick, '@')) || *user_host_or_nick == '*')
+  nuh.nuhmask  = user_host_or_nick;
+  nuh.nickptr  = lnick;
+  nuh.userptr  = luser;
+  nuh.hostptr  = lhost;
+
+  nuh.nicksize = NICKLEN;
+  nuh.usersize = USERLEN*4+1;
+  nuh.hostsize = HOSTLEN*4+1;
+
+  split_nuh(&nuh);
+
+  if (!(flags & NOUSERLOOKUP) && irccmp(lnick, "*"))
   {
-    struct split_nuh_item nuh;
+    const struct Client *target_p = NULL
 
-    nuh.nuhmask  = user_host_or_nick;
-    nuh.nickptr  = NULL;
-    nuh.userptr  = luser;
-    nuh.hostptr  = lhost;
-
-    nuh.nicksize = 0;
-    nuh.usersize = USERLEN*4+1;
-    nuh.hostsize = HOSTLEN*4+1;
-
-    split_nuh(&nuh);
-
-    return 1;
-  }
-  else if (!(flags & NOUSERLOOKUP))
-  {
     /* Try to find user@host mask from nick */
     /* Okay to use source_p as the first param, because source_p == client_p */
-    if ((target_p = 
-        find_chasing(source_p, source_p, user_host_or_nick, NULL)) == NULL)
+    if ((target_p = find_chasing(source_p, source_p, lnick, NULL)) == NULL)
       return 0;
 
     if (IsExemptKline(target_p))
     {
       if (!IsServer(source_p))
-	sendto_one(source_p,
-		   ":%s NOTICE %s :%s is E-lined",
-		   me.name, source_p->name, target_p->name);
+        sendto_one(source_p, ":%s NOTICE %s :%s is E-lined",
+                   me.name, source_p->name, target_p->name);
       return 0;
     }
 
@@ -332,15 +327,14 @@ find_user_host(struct Client *source_p, char *user_host_or_nick,
     if (target_p->username[0] == '~')
       luser[0] = '*';
 
-    if (target_p->sockhost[0] == '\0' ||
-        (target_p->sockhost[0] == '0' && target_p->sockhost[1] == '\0'))
+    if (target_p->sockhost[0] == '\0' || !irccmp(target_p->sockhost, "0"))
       strlcpy(lhost, target_p->host, HOSTLEN*4 + 1);
     else
       strlcpy(lhost, target_p->sockhost, HOSTLEN*4 + 1);
     return 1;
   }
 
-  return 0;
+  return 1;
 }
 
 /*
