9a6bc930fe51d75d1225606f52c0edd5ec9202ea
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2006-02-27 14:52:35 +0000

    - Don't give away IPs unless it's an admin doing /stats P.
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid@483 82007160-df01-0410-b94d-b575c5fd34c7

--- src/listener.c
+++ src/listener.c
@@ -115,7 +115,7 @@ show_ports(struct Client *source_p)
     *p = '\0';
     sendto_one(source_p, form_str(RPL_STATSPLINE),
                me.name, source_p->name, 'P', listener->port,
-               listener->name,
+               IsAdmin(source_p) ? listener->name : me.name,
                listener->ref_count, buf,
                listener->active ? "active" : "disabled");
   }
