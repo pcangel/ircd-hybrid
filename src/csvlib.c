879c24f00cbe296bcb2e7f5bb179d21e02a2fc81
db <db@82007160-df01-0410-b94d-b575c5fd34c7>
2006-03-11 21:36:58 +0000

    - When checking for first " in a line, make sure it doesn't run off   the end of the line before it finds one.
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid@520 82007160-df01-0410-b94d-b575c5fd34c7

--- src/csvlib.c
+++ src/csvlib.c
@@ -517,8 +517,15 @@ getfield(char *newline)
 
   field = line;
 
-  while (*field != '"')	/* skip everything that's not a starting quote */
+  /* skip everything that's not a starting quote */
+  for(;;)
+  {
+    if (*field == '\0')
+      return(NULL);
+    else if (*field == '"')
+      break;
     ++field;
+  }
 
   /* skip over the beginning " */
   end = ++field;
