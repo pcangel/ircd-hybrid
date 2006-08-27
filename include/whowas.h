317eb389461168eb8c72627a9510d61c5be81e67
michael <michael@82007160-df01-0410-b94d-b575c5fd34c7>
2006-08-27 10:07:17 +0000

    - Doxyfied whowas.h
    git-svn-id: svn://svn.ircd-hybrid.org/svnroot/ircd-hybrid@789 82007160-df01-0410-b94d-b575c5fd34c7

--- include/whowas.h
+++ include/whowas.h
@@ -1,6 +1,5 @@
 /*
  *  ircd-hybrid: an advanced Internet Relay Chat Daemon(ircd).
- *  whowas.h: Header for the whowas functions.
  *
  *  Copyright (C) 2002 by the past and present ircd coders, and others.
  *
@@ -18,25 +17,29 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
  *  USA
- *
- *  $Id$
+ */
+
+/*! \file whowas.h
+ * \brief Maintains a history of nick names
+ * \version $Id$
  */
 
 #ifndef INCLUDED_whowas_h
 #define INCLUDED_whowas_h
 
+/*! \brief Whowas structure */
 struct Whowas
 {
-  int hashv;
-  time_t logoff;
-  char name[NICKLEN];
-  char username[USERLEN + 1]; 
-  char hostname[HOSTLEN + 1];
-  char realname[REALLEN + 1];
-  char servername[HOSTLEN + 1];
-  struct Client *online; /* Pointer to new nickname for chasing or NULL */
-  dlink_node tnode;  /* for hash table... */
-  dlink_node cnode; /* for client struct linked list */
+  int hashv; /**< value of the hashed nick name */
+  time_t logoff; /**< When the logged off from irc */
+  char name[NICKLEN]; /**< Client's nick name */
+  char username[USERLEN + 1]; /**< Client's user name */
+  char hostname[HOSTLEN + 1]; /**< Client's host name */
+  char realname[REALLEN + 1]; /**< Client's real name (Gecos) */
+  char servername[HOSTLEN + 1]; /**< Name of the server the client is/was on */
+  struct Client *online; /**< Pointer to new nickname for chasing or NULL */
+  dlink_node tnode;  /**< for hash table... */
+  dlink_node cnode; /**< for client struct linked list */
 };
 
 EXTERN void whowas_init(void);
